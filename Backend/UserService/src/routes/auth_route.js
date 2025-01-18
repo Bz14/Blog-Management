7d87160bb74be22c7bda5abab340bf4ee731cdf6
Bz14 <bezueyerusalem@gmail.com>
2025-01-18 17:35:00 +0300

    Folder structure setup for microservice

--- Backend/UserService/src/routes/auth_route.js
+++ Backend/UserService/src/routes/auth_route.js
@@ -1,7 +1,7 @@
 const route = require("express").Router();
 const AuthController = require("../controllers/auth_controller");
 const AuthRepo = require("../repository/auth_repo");
-const AuthService = require("../service/auth_service");
+const AuthService = require("../../../service/auth_service");
 
 const authRepo = new AuthRepo();
 const authService = new AuthService(authRepo);
